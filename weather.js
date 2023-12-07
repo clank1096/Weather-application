d1d9716c3d57f7f7fbb1c1f4d5cabed93d4ce920
clank1096 <92971016+clank1096@users.noreply.github.com>
2023-12-07 19:06:59 +0200

    Tehty kuva muunnoksia
    Lisätty koodiin kuva muunnoksia kaupungin kuviksi kun vaihdetaan.
    Drop down menu muutettu vasen yläkulmaan.
    Otsikko keski ylhäällä
    Lisätty tekstiä sää tiedotteeseen

--- weather.js
+++ weather.js
@@ -1,25 +1,25 @@
 function vaihdaKaupunki(KaupunginNimi) { 
     
     let apiUrl = '';
-    
+    let taustaKuvat = './Images/sunny.jpg';
     
     switch (KaupunginNimi) {
       case 'Helsinki':
         apiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=60.1695&longitude=24.9354&hourly=temperature_2m';
+        taustaKuvat = "./Images/helsinki.png";
         break;
       case 'Vaasa':
         apiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=63.096&longitude=21.6158&hourly=temperature_2m';
+        taustaKuvat = "./Images/Vaasa.png";
         break;
       case 'Laihia':
         apiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=62.9761&longitude=22.0114&hourly=temperature_2m';
+        taustaKuvat = "./Images/Laihia.png"; //Olisipa parempia kuvia....
         break;
         case 'Seinäjoki':
         apiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=62.7945&longitude=22.8282&hourly=temperature_2m';
+        taustaKuvat = "./Images/Seinäjoki.png";
         break; 
-        
-        
-    
-      
     }
   
 
@@ -32,7 +32,13 @@ function vaihdaKaupunki(KaupunginNimi) {
         
         
         locationElement.textContent = KaupunginNimi; 
-        temperatureElement.textContent = data.hourly.temperature_2m[0]+ '°C'; 
-     
+        temperatureElement.textContent = 'Kaupungin nykyinen sää on: ' + data.hourly.temperature_2m[0]+ '°C'; 
+        
+        //tausta kuva
+        document.body.style.backgroundImage = `url(${taustaKuvat})`; // <---- hakkasin päätä ton kanssa kuulemma ei saa käyttää ' vaan joutuu käyttää jotai ` -.-
+        
       });
   }
+
+
+  
